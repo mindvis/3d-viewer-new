6757ed931416b5cda4982c0212686aae4b7d7d78
mindvis <himanshu.vasistha@gmail.com>
2023-09-10 23:42:00 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-import * as THREE from 'three';
+import * as THREE from './three';
 import { OrbitControls } from './three/addons/controls/OrbitControls.js';
 import { GLTFLoader } from './three/addons/loaders/GLTFLoader.js';
 import { EffectComposer } from './three/addons/postprocessing/EffectComposer.js';
